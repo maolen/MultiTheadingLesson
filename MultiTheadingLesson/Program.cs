ff53d1188aa3211673190bab0fc0517d2188583d
Kamila Zhakupova <zhakupova.kamila@gmail.com>
2019-12-04 19:38:30 +0600

    Создан второй поток

--- MultiTheadingLesson/Program.cs
+++ MultiTheadingLesson/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace MultiTheadingLesson
 {
@@ -6,7 +7,31 @@ namespace MultiTheadingLesson
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var currentThread = Thread.CurrentThread;
+            //Console.WriteLine($"{currentThread.ManagedThreadId} - айди потока");
+            Console.WriteLine($"Поток {currentThread.ManagedThreadId} начал работу");
+
+            //// Имитация длительной работы.
+            //Thread.Sleep(5000);
+
+            var thread = new Thread(ProcessWork);
+            thread.Start();
+
+            ProcessWork();
+
+            Console.WriteLine($"Поток {currentThread.ManagedThreadId} закончил работу");
+            Console.ReadLine();
+        }
+
+        static void ProcessWork()
+        {
+            var currentThread = Thread.CurrentThread;
+
+            for (var i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"[{currentThread.ManagedThreadId}] - {i}");
+                Thread.Sleep(500);
+            }
         }
     }
 }
